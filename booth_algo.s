94755ebd6becf3c19f8a1312fe2b122013898f66
Amrut182 <rxyamrutu@gmail.com>
2020-10-07 04:26:50 +0530

    pushing changes

--- booth_algo.s
+++ booth_algo.s
@@ -1,8 +1,8 @@
-	AREA DATA1, DATA; blah
-A EQU 0xB
+	AREA DATA1, DATA
+A EQU 0x0
 q EQU 0x0 ;Q-1 bit
 M EQU 0x5
-Q EQU 0X5
+Q EQU 0X6
 N EQU 0X4
 w EQU 0x00; ASR
 x EQU 0x03; ASR
@@ -30,6 +30,14 @@ label1
 label2
 	MOV R3, 1
 	MOV PC, R9
+	
+label3
+	MOV R2, R2, lsr 1; right shift Q
+	MOV PC, R9
+
+label4
+	MOV R3, 0
+	MOV PC, R9
 AQq
 	and R5, R1, 1; getting LSB of A
 	MOV R1, R1, lsr 1
@@ -38,11 +46,17 @@ AQq
 	;stmfd sp!, {list of registers }
 	add R9, PC, 4 
 	beq label1
+	add R9, PC, 4 
+	bne label3
+	
 	add R1, R1, R5
 	and R5, R6, 1; getting LSB of Q
+	cmp R5, 1
 	MOV R9, 0
 	MOV R9, PC
 	beq label2
+	MOV R9, PC
+	bne label4
 	;debug
 	MOV R1, R1, lsl R10
 	add R1, R1, R2
@@ -50,14 +64,40 @@ AQq
 	add R1, R1, R3
 	;
 	BX LR
-	
+
+; A = A - M
+ylabel
+	MVN R4, R4
+	add R4, R4, 1
+	add R1, R1, R4
+	BX LR
+; A = A + M
+zlabel
+	add R1, R1, R4
+	BX LR
+;endlabel
+	;BX LR
 __START
 	MOV R1,A; Accumulator
-	MOV R2,Q; Multiplier
+	MVN R2,Q; Multiplier
+	add R2, R2, 1
+	;sub R2, R2, Q; Multiplier (for negative)
 	MOV R3,q; Booth Bit
 	MOV R4, M; Mulitplicand
 	MOV R10, N; Count
+;loop
 	BL getTwoBit
+	
+	cmp R5, y
+	beq ylabel
+	 
+	cmp R5, z
+	beq zlabel
+	
 	BL AQq
+	sub R10, R10, 1
+	cmp R10, 0
+	;bne loop
+	;beq endlabel
 LABEL B LABEL		
 	END
